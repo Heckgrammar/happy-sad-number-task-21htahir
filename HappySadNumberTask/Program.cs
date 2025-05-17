cae2bcd99433af8d2aa6ee128af96fa91408b5fe
21htahir <144434579+21htahir@users.noreply.github.com>
2025-05-17 11:37:29 +0100

    Update Program.cs

--- HappySadNumberTask/Program.cs
+++ HappySadNumberTask/Program.cs
@@ -1,30 +1,53 @@
-﻿namespace HappySadNumberTask
+using System;
+using System.Collections.Generic;
+
+namespace HappySadNumberTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*There are said to be happy numbers and sad numbers.
-            Happy numbers will reduce to 1 when the digits belonging to the numbers are squared and added together to produce another number and the process is repeated. e.g. when we take 19
-            1 squared is 1 , 9 squared is 81. 81 +1 = 82
-            8 squared is 64, 2 squared is 4.  64 + 4 = 68
-            6 squared is 36, 8 squared is 64, 36 + 64 = 100 
-            1 squared is 1, 0 squared is 0 and thus the original number is reduced to 1
-            Sad numbers will not reduce to 1 and will loop endlessly. e.g. when we take 20
-            2 squared is 4, 0 squared is 0, 4 + 0 = 4
-            4 squared is 16, 0 squared is 0, 16 + 0 = 16
-            1 squared is 1, 6 squared is 36, 1 + 36 = 37
-            3 squared is 9, 7 squared is 49, 9 + 49 = 58
-            5 squared is 25, 8 squared is 64, 25 + 64 = 89
-            8 squared is 64, 9 squared is 81, 64 + 81 = 145
-            1 squared is 1, 4 squared is 16, 5 squared is 25, 1 + 16 + 25 = 42
-            4 squared is 16, 2 squared is 4, 16 + 4 = 20
-            20 will loop endlessly
-            */
-            //write a program to determine if a number is happy or sad
-            //The program should take a number as input and output whether the number is happy or sad
-            //test your program with the inputs of 19 and 20
-            //show your output in the readme file
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (IsHappyNumber(number))
+                {
+                    Console.WriteLine($"{number} is a Happy number!");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is a Sad number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+            }
+        }
+
+        static bool IsHappyNumber(int num)
+        {
+            HashSet<int> seenNumbers = new HashSet<int>();
+
+            while (num != 1 && !seenNumbers.Contains(num))
+            {
+                seenNumbers.Add(num);
+                num = GetSumOfSquares(num);
+            }
+
+            return num == 1;
+        }
+
+        static int GetSumOfSquares(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit * digit;
+                number /= 10;
+            }
+            return sum;
         }
     }
 }
